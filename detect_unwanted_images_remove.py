4b0231aa9c52d0c57d563bdbdabae305418f3f6b
harshulmalhotra <33556967+harshulmalhotra@users.noreply.github.com>
2019-01-18 12:26:20 +0530

    Update detect_unwanted_images_remove.py

--- detect_unwanted_images_remove.py
+++ detect_unwanted_images_remove.py
@@ -3,12 +3,12 @@ from PIL import Image
 from pytesseract import image_to_string
 
 for index, filename in  enumerate(os.listdir('.')):  #listdir('.') = current directory
-        if filename.endswith(".py"):
+        if filename.endswith(".py"):     #some python files can also be in directory..so to ignore that
             continue
         img=Image.open(filename)
         result=image_to_string(img,lang="eng")
     
-        if "morning" in result.lower():
+        if "morning" in result.lower():      #detecting good morning images
             os.remove(filename)
-        elif len(result)==0:
+        elif len(result)==0:            #most memes,unwanted images have no text which can extracted..so this!!
             os.remove(filename)
