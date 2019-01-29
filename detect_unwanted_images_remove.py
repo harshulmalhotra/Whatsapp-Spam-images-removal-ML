ca126b78f90e99426804dce0e6d8442c5044846f
peachy <dillonpie@gmail.com>
2019-01-28 23:41:48 -0700

    Capturing black text now!
    This code now takes black text and transforms it into a picture with
    white text on a black background, increasing the ease of Pytesseract to
    recognize characters!

--- detect_unwanted_images_remove.py
+++ detect_unwanted_images_remove.py
@@ -1,26 +1,35 @@
 import os
 from PIL import Image, ImageFile
-from pytesseract import image_to_string
+from pytesseract import image_to_string, image_to_data
+import numpy as np
+import cv2
+
 
 image_path = 'images\\'
 ImageFile.LOAD_TRUNCATED_IMAGES = True
+tessdata_dir = r'"C:\Users\MrJus\Documents\Whatsapp-Spam-images-removal-ML"'
 
 for filename in os.listdir(image_path):  # listdir('.') = current directory
     print(os.path.join(image_path, filename))
-    img = Image.open(os.path.join(image_path, filename))
-    img.load()
+
+    im = cv2.imread(os.path.join(image_path, filename))
+
+    im[np.where((np.logical_and(im >= [0, 0, 0], im <= [20, 20, 20]))
+                .all(axis=2))] = [0, 33, 166]
+    im[np.where((im > [20, 20, 20])
+                .all(axis=2))] = [255, 255, 255]
+    im[np.where((im == [0, 33, 166])
+                .all(axis=2))] = [0, 0, 0]
+
     try:
-        result = image_to_string(img, lang='eng',
-                                 config='-c '
-                                        'tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnozqrstuvwxyz,.')
+        result = image_to_string(im, lang='eng',
+                                 config=tessdata_dir + ' -c '
+                                        "tessedit_char_whitelist=\\'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnozqrstuvwxyz,.")
         if len(result) > 0:
-            print(result)
+             print(result)
     except IOError:
         print("Problem with file I/O")
 
-
-
-    # if "morning" in result.lower():  # detecting good morning images
-    #     os.remove(filename)
-    # elif len(result) == 0:  # most memes,unwanted images have no text which can extracted..so this!!
-    #     os.remove(filename)
+    cv2.imshow('image', im)
+    cv2.waitKey(5000)
+    cv2.destroyAllWindows()
